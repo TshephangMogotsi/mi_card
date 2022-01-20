2c96c374d9f26a1924242ed33e6ddcd406c3c050
TshephangMogotsi <zinjie7@gmail.com>
2022-01-20 09:21:14 +0200

    added font files and changed font in the UI

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,29 @@ class MyApp extends StatelessWidget {
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                CircleAvatar(
+              children:  [
+                const CircleAvatar(
                   radius: 50.0,
                   backgroundImage: AssetImage("images/best-doggo.jpg"),
                 ),
-                Text(
-                  "bubbles",
+                const Text(
+                  "Bubbles",
                   style: TextStyle(
+                    fontFamily: 'Pacifico',
                     fontSize: 40.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold
                   ),
-                )
+                ),
+                Text(
+                  "BEST DOG EVER",
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.normal
+                  ),
+                ),
               ]
             ),
           ),
