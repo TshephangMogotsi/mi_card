d10c0cac187c32c13c3c10ab0c069cc56d0ce3ab
TshephangMogotsi <zinjie7@gmail.com>
2022-01-20 10:19:40 +0200

    replaced containers with list tiles, added a divider

--- lib/main.dart
+++ lib/main.dart
@@ -14,35 +14,73 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:  [
-                const CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage("images/best-doggo.jpg"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, 
+          children: [
+              const CircleAvatar(
+          radius: 50.0,
+          backgroundImage: AssetImage("images/best-doggo.jpg"),
+              ),
+              const Text(
+          "Bubbles",
+          style: TextStyle(
+              fontFamily: 'Pacifico',
+              fontSize: 40.0,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
+              ),
+              Text(
+          "BEST DOG EVER",
+          style: TextStyle(
+              fontFamily: 'Source Sans Pro',
+              fontSize: 20.0,
+              color: Colors.teal.shade100,
+              fontWeight: FontWeight.normal),
+              ),
+              SizedBox(
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
                 ),
-                const Text(
-                  "Bubbles",
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                Text(
-                  "BEST DOG EVER",
-                  style: TextStyle(
-                    fontFamily: 'Source Sans Pro',
-                    fontSize: 20.0,
-                    color: Colors.teal.shade100,
-                    fontWeight: FontWeight.normal
-                  ),
-                ),
-              ]
+              ),
+              Card(
+          color: Colors.white,
+          margin: const EdgeInsets.symmetric(
+              vertical: 10.0, horizontal: 25.0),
+          child: ListTile(
+            leading: const Icon(
+              Icons.phone,
+              color: Colors.teal,
+            ),
+            title: Text(
+              "+267 74271668",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.teal.shade900,
+              ),
             ),
           ),
+              ),
+              Card(
+          color: Colors.white,
+          margin: const EdgeInsets.symmetric(
+              vertical: 10.0, horizontal: 25.0),
+          child: ListTile(
+            leading: const Icon(
+              Icons.email,
+              color: Colors.teal,
+            ),
+            title: Text(
+              "bubbles01@gmail.com",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.teal.shade900,
+              ),
+            ),
+          ),
+              ),
+              
+            ]),
         ),
       ),
     );
